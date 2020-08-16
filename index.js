686693defd147eadf8c747c1589c0c495489c5ff
michael thomas <michael.thomas2014@gmail.com>
2020-08-16 18:59:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
- 
+  let right =  parseInt(rightNumbers, 10);
   if (left < 0) {
     dodger.style.left = `${left + 1}px`;
   }
